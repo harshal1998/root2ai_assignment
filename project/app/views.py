2113a0e4dba08fb00469458bb86b7cd27e5e9eeb
harshal1998 <harshalwankhede1998@gmail.com>
2021-06-17 01:06:15 +0530

    changes for adding data using django framework

--- project/app/views.py
+++ project/app/views.py
@@ -3,24 +3,24 @@ from .models import *
 import pandas as pd
 import csv
 import psycopg2
-
+import numpy as np
 
 with open('data.csv', 'r') as f:
     reader = csv.reader(f)
-    next(reader)
-    # Skip the header row.
+    c = len(next(reader))
+
     for i in reader:
         temp = demo.objects.values_list('a', flat=True)
         if i[0] in temp:
             continue
         else:
-            d = demo(a=i[0], b=i[1], c=i[2], d=i[3], e=i[4], f=i[5], g=i[6], h=i[7], i=i[8], j=i[9], k=i[10], l=i[11],
-                     m=i[12], n=i[13], o=i[14], p=i[15], q=i[16], r=i[17], s=i[18])
+            d = demo(a=i[c - 19], b=i[c - 18], c=i[c - 17], d=i[c - 16], e=i[c - 15], f=i[c - 14], g=i[c - 13],
+                     h=i[c - 12], i=i[c - 11], j=i[c - 10], k=i[c - 9], l=i[c - 8], m=i[c - 7], n=i[c - 6], o=i[c - 5],
+                     p=i[c - 4], q=i[c - 3], r=i[c - 2], s=i[c - 1])
             d.save()
 
 
 def display(request):
     # a = pd.read_sql_query('select * from app_demo', con=engine)
     data = demo.objects.all()
-    # print(a)
     return render(request, 'a.html', {"data": data})
