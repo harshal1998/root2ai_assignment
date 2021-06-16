761347ab727b269e17384ba7a907dfe5182a4293
harshal1998 <harshalwankhede1998@gmail.com>
2021-06-16 20:21:35 +0530

    changes for adding data using django framework

--- project/app/views.py
+++ project/app/views.py
@@ -4,17 +4,19 @@ import pandas as pd
 import csv
 import psycopg2
 
-conn = psycopg2.connect("host=localhost dbname=root2ai user=postgres password=root")
-cur = conn.cursor()
+
 with open('data.csv', 'r') as f:
     reader = csv.reader(f)
     next(reader)
- # Skip the header row.
-    for row in reader:
-        cur.execute(
-            "INSERT INTO app_demo (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",row
-        )
-conn.commit()
+    # Skip the header row.
+    for i in reader:
+        temp = demo.objects.values_list('a', flat=True)
+        if i[0] in temp:
+            continue
+        else:
+            d = demo(a=i[0], b=i[1], c=i[2], d=i[3], e=i[4], f=i[5], g=i[6], h=i[7], i=i[8], j=i[9], k=i[10], l=i[11],
+                     m=i[12], n=i[13], o=i[14], p=i[15], q=i[16], r=i[17], s=i[18])
+            d.save()
 
 
 def display(request):
